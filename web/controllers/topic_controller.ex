ef54b53aa2bfae8fd6d83263b9ab90450d6ae4b2
Liam Besaw <lbesaw@wgu.edu>
2017-04-04 23:50:10 -0700

    first commit

---
+++ web/controllers/topic_controller.ex
@@ -0,0 +1,84 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+
+  alias Discuss.Topic
+
+  plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+  plug :check_topic_owner when action in [:update, :edit, :delete]  #applies this check_topic_owner method to each of the listed actions
+
+  def edit(conn, %{"id" => topic_id}) do
+     topic = Repo.get(Topic, topic_id)
+     changeset = Topic.changeset(topic)
+
+     render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def delete(conn, %{"id" => topic_id}) do
+    Repo.get!(Topic, topic_id) |> Repo.delete!
+
+    conn
+    |> put_flash(:info, "Topic Deleted")
+    |> redirect(to: topic_path(conn, :index))
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "edit.html", changeset: changeset, topic: old_topic
+    end
+  end
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+
+    changeset = conn.assigns.user  #this grabs the user from the conn object
+    |> build_assoc(:topics)        #this builds the association between the user object and the topic being created
+    |> Topic.changeset(topic)      #finally this builds the changeset by passing the user and new topic into the changeset
+
+    case Repo.insert(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
+  end
+
+  def show(conn, %{"id" => topic_id}) do
+
+    topic = Repo.get!(Topic, topic_id)
+    render conn, "show.html", topic: topic
+
+  end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You cannot edit that")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt()
+    end
+  end
+
+end
